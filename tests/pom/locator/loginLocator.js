23dd6c5272c17f29a9a595acab0290a80890fd16
aldojulian <aldojuliannn@gmail.com>
2025-04-21 10:07:12 +0700

    push 3 dan 4

---
+++ tests/pom/locator/loginLocator.js
@@ -0,0 +1,13 @@
+export default class loginLocator {
+    inputUsername = '#user-name';
+    inputPassword = '#password';
+    buttonLogin = '#login-button';
+    buttonAddToCart = '#add-to-cart-sauce-labs-backpack';
+    buttonCart = '#shopping_cart_container > a';
+    buttonCheckout = '#checkout';
+    inputFirstName = '#first-name';
+    inputLastName = '#last-name';
+    inputPostalCode = '#postal-code';
+    buttonContinue = '#continue';
+    buttonFinish = '#finish';
+}
